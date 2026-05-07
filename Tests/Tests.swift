41ea87fdfaa4a508ff517dcb4aef6ae7f8fb864f
Konstantin Sukharev <poed@me.com>
2026-05-07 14:27:06 +0200

    Update randomDistribution test

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -7,14 +7,18 @@ struct Tests {
 		var d20 = D20()
 		var bins = [20 of UInt16](repeating: 0)
 
-		(0 ..< Int(UInt16.max)).forEach { i in
+		let throwsCount = 65_000
+		let expected = throwsCount / 21
+
+		(0 ..< throwsCount).forEach { i in
 			bins[d20()] += 1
 		}
 
 		let str = bins.map { "\($0)" }.joined(separator: ", ")
-		print("bins: \(str)")
+		print("Bins: \(str)")
+		print("Each bin is expected to be at least: \(expected)")
 		#expect(bins.indices.reduce(true) { r, i in
-			r && bins[i] > 3000
+			r && bins[i] > expected
 		})
     }
 }
